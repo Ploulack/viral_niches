cc7d3b8adfaa3360bcac79bf118fad28371ed554
Ploulackk <mathieu.castelli@gmail.com>
2021-02-28 00:08:00 +0100

    Completed pairs table

--- niche_viral.R
+++ niche_viral.R
@@ -1,19 +1,16 @@
 library(tidyverse); library("DECIPHER")
 
-
-
-
 dist_vir <- readDNAStringSet("data/ASVs.fa") %>% 
 	OrientNucleotides() %>% 
 	DistanceMatrix() %>%
 	as_tibble(rownames = "vir_A") %>%
 	pivot_longer(names_to = "vir_B",
 				 cols = -1,
-				 values_to = "distance")
-
-covar_virbact <- read_csv("data/virbact.cov.csv") %>% 
-	select(-X1) %>% 
-	rename(weight = "cor")
+				 values_to = "distance") %>% 
+	mutate(across(.cols = 1:2,
+				  ~ .x %>% 
+				  	str_remove("ASV_") %>% 
+				  	as.integer()))
 
 covar_virvir <- read_csv("data/virvir.covar.csv") %>% 
 	select(-X1)
@@ -25,16 +22,26 @@ vir_all <- covar_virvir %>%
 	select(vir_A, vir_B, distance, cov = weight)
 
 vir_all %>% write_csv("export/vir_all.csv")
+	
+covar_virbact <- read_csv("data/virbact.cov.csv") %>% 
+	select(-X1) %>% 
+	rename(weight = "covar_vir_bact", from = "vir", to = "bact") %>% 
+	mutate(vir = vir %>% str_remove("vir_ASV_") %>% as.integer())
 
+vir_cov_dist <- covar_virbact %>% 
+	group_by(bact) %>%
+	# Restrict the analysis to bact for which we have at least 7 vir covariances
+	filter(n() >= 7) %>% 
+	group_modify(~{
+		.x %>% 
+			# Generate pairs
+			tidyr::expand(v1 = vir,v2 = vir) %>% 
+			# Eliminate duplicates
+			filter(v1 < v2) %>% 
+			# Reattach covar with current bact
+			left_join(.x, by = c("v1" = "vir")) %>% 
+			left_join(.x, by = c("v2" = "vir"), suffix = c("1", "2")) %>% 
+			# Attach distance
+			left_join(dist_vir, by = c("v1" = "vir_A", "v2" = "vir_B"))
+	})
 
-
-
-
-
-# test <- cor_virbact %>% 
-# 	mutate(vir = from %>% str_extract("(?<=vir_ASV_)\\d*"),
-# 		   bact = to %>% str_extract("(?<=ASV_)\\d*"))
-
-# test$vir %>% 
-# 	map_lgl(~{. %in% test$bact}) %>% 
-# 	sum()
