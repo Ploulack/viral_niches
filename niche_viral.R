b2214ec890789b775f144f28ceda19700143eac8
Ploulackk <mathieu.castelli@gmail.com>
2021-03-07 22:31:55 +0100

    with correlation

--- niche_viral.R
+++ niche_viral.R
@@ -18,11 +18,12 @@ covar_virvir <- read_csv("data/virvir.covar.csv") %>%
 vir_all <- covar_virvir %>% 
 	mutate(vir_A = pmin(from, to),
 		   vir_B = pmax(from, to)) %>%
+	mutate(across(-weight, ~str_remove(.x, "ASV_") %>% as.integer())) %>% 
 	left_join(dist_vir) %>% 
 	select(vir_A, vir_B, distance, cov = weight)
 
 vir_all %>% write_csv("export/vir_all.csv")
-	
+
 covar_virbact <- read_csv("data/virbact.cov.csv") %>% 
 	select(-X1) %>% 
 	rename(weight = "covar_vir_bact", from = "vir", to = "bact") %>% 
@@ -33,7 +34,8 @@ vir_cov_dist <- covar_virbact %>%
 	# Restrict the analysis to bact for which we have at least 7 vir covariances
 	filter(n() >= 7) %>% 
 	group_modify(~{
-		.x %>% 
+		print(.y$bact)
+		data <- .x %>% 
 			# Generate pairs
 			tidyr::expand(v1 = vir,v2 = vir) %>% 
 			# Eliminate duplicates
@@ -42,7 +44,18 @@ vir_cov_dist <- covar_virbact %>%
 			left_join(.x, by = c("v1" = "vir")) %>% 
 			left_join(.x, by = c("v2" = "vir"), suffix = c("1", "2")) %>% 
 			# Attach distance
-			left_join(dist_vir, by = c("v1" = "vir_A", "v2" = "vir_B"))
-	})
+			left_join(dist_vir, by = c("v1" = "vir_A", "v2" = "vir_B")) %>% 
+			mutate(abs_diff = abs(covar_vir_bact1 - covar_vir_bact2))
+		
+		res <- cor.test(x = data$distance, y = data$abs_diff)
+		
+		tibble(n_vir = nrow(.x),
+			   n_pairs = nrow(data),
+			   estimate = res$estimate,
+			   p_value = res$p.value,
+			   data = list(data))
+	}) %>% 
+	ungroup()
 
-vir_cov_dist %>% write_csv("export/vir_cov_dist")
+save.image("vir_cov_done.RData")
+vir_cov_dist %>% filter(p_value < .05)
